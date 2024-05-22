befaa2d71595c146fbdf7694f227594ac5dd8d7a
Melisa Rivera <melisa.rivera@utec.edu.pe>
2024-05-21 21:56:08 -0500

    final

--- judge.cc
+++ judge.cc
@@ -1,11 +1,8 @@
 #include <string>
 #include <iostream>
-<<<<<<< HEAD
 #include "Melisa.h"
-=======
 #include <memory>
 #include <vector>
->>>>>>> origin/main
 
 #include "Player.h"
 #include "example_player.h"
@@ -50,14 +47,7 @@ int status(Board const& b)
     return NOT_ENDED;
 }
 
-<<<<<<< HEAD
-bool isFull(Board const& b)
-{
-    for (int i = 0; i < 9; ++i)
-        if (b[i] == ' ')
-            return false;
-    return true;
-=======
+
 int play_game(Player *p_X, Player *p_O)
 {
     Board b{};
@@ -77,57 +67,17 @@ int play_game(Player *p_X, Player *p_O)
         whomoves = ((whomoves == 'X') ? 'O' : 'X');
     }
     return status(b);
->>>>>>> origin/main
 }
 
 int main()
 {
-<<<<<<< HEAD
-     MyStrategy p = MyStrategy();
-    Board b;
-
-    //inicialiar b con espacios
-    for (int i = 0; i < 9; ++i) {
-        b[i] = ' ';
-    }
-
-    char turn_of = 'X';
-    int count = 0;
-    while (true) {
-        p.print_board(b);
-        if (count % 2 == 0) {
-            //soliciatar movimiento al usuario
-            int move;
-            std::cout << "Ingrese su movimiento: ";
-            std::cin >> move;
-            b[move] = turn_of;
-        } else {
-            //solicitar movimiento a la computadora
-            int move = p.get_move(b, turn_of);
-            b[move] = turn_of;
-        }
-        
-        if (won(b, turn_of)) {
-            p.print_board(b);
-            std::cout << "Ganador: " << turn_of << std::endl;
-            break;
-        }
-        if (isFull(b)) {
-            p.print_board(b);
-            std::cout << "Empate" << std::endl;
-            break;
-        }
-        turn_of = (turn_of == 'X') ? 'O' : 'X';
-        count++;
-    }
-=======
     std::vector<Player*> players;
     std::vector<std::string> names;
 
     players.push_back(new Example_player());
     names.push_back("Example_player");
-    players.push_back(new Example_player());
-    names.push_back("Example_player");
+    players.push_back(new Melisa());
+    names.push_back("Melisa");
 
     for (int i =0; i < players.size(); ++i)
         for (int j = i + 1; j < players.size(); ++j)
@@ -144,6 +94,5 @@ int main()
                 std::cout << "  " << "Not handled result" << std::endl;
         }
 
->>>>>>> origin/main
     return 0;
 }
