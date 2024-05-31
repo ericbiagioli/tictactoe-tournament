7669317135dd64b44f41ae96f79f231596e21d96
Roger Huauya Mamani <38589877+RogerHuauya@users.noreply.github.com>
2024-05-31 00:07:41 -0500

    Competition working (#1)
    * Competition working
    
    * Fix rebasing
    
    * adding winning strategy
    
    * Chore: change name files
    
    * rebase
    
    ---------
    
    Co-authored-by: Eric Biagioli <ericbiagioli@gmail.com>

---
+++ Roger.h
@@ -0,0 +1,13 @@
+#ifndef example_player_h
+#define example_player_h
+
+#include <vector>
+#include "Player.h"
+
+struct Example_player : public Player
+{
+    Example_player() {}
+    int get_move(Board const& b, char token);
+};
+
+#endif
